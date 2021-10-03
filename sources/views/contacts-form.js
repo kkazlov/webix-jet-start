a422254992c9acf8e321f369dd7339fd60ae9a16
kkazlov <kkazlov.it@gmail.com>
2021-10-03 17:51:03 +0300

    less2 - task2 done

--- sources/views/contacts-form.js
+++ sources/views/contacts-form.js
@@ -1,13 +1,39 @@
 import { JetView } from "webix-jet";
 import { contactsCollection } from "../models/dataCollections";
+import { countries } from "../models/countries";
+import { statuses } from "../models/statuses";
 
 export default class ContactsForm extends JetView {
 	config() {
+		const comboCountries = {
+			view: "combo",
+			label: "Countries",
+			options: {
+				body: {
+					template: "#Name#",
+					data: countries,
+				},
+			},
+		};
+
+		const comboStatuses = {
+			view: "combo",
+			label: "Statuses",
+			options: {
+				body: {
+					template: "#Name#",
+					data: statuses,
+				},
+			},
+		};
+
 		return {
 			view: "form",
 			elements: [
 				{ view: "text", label: "Name", name: "Name" },
 				{ view: "text", label: "Email", name: "Email" },
+				comboCountries,
+				comboStatuses,
 				{
 					cols: [
 						{ view: "button", value: "Save", css: "webix_primary" },
